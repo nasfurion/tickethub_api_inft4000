5bcc8dc126aee97e2bdc63edc56c42b7539edf6c
Nasfurion <diego.amadio93@gmail.com>
2025-03-26 15:27:23 -0300

    Initial Commit

---
+++ TicketHub_API/Controllers/PurchaseController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketHub_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PurchaseController : ControllerBase
+    {
+        private readonly ILogger<PurchaseController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public PurchaseController(ILogger<PurchaseController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the purchase to the database
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ TicketHub_API/Purchase.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketHub_API
+{
+    public class Purchase
+    {
+        [Required]
+        public int concertId { get; set; }
+
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Please enter a valid Email address")]
+        public string email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "The name inserted exceeds the maximum character length")]
+        public string name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Phone(ErrorMessage = "Please enter a valid phone number")]
+        public string phone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Quantity is required")]
+        [Range(1, 6, ErrorMessage = "The allowed ticket quantity is between 1 and 6")]
+        public int quantity { get; set; }
+
+        [Required(ErrorMessage = "Credit card number is required")]
+        [CreditCard(ErrorMessage = "Please enter a valid credit card number")]
+        public string creditCard { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Expiration date is required")]
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$", ErrorMessage = "Please enter a valid expiration date")]
+        public string expiration { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Security code is required")]
+        [StringLength(4, MinimumLength = 3, ErrorMessage = "Please enter a valid security code")]
+        public string securityCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Address is required")]
+        public string address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City is required")]
+        public string city { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Province is required")]
+        public string province { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Postal code is required")]
+        //https://stackoverflow.com/questions/1146202/canadian-postal-code-validation
+        [RegularExpression(@"[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]", ErrorMessage = "Please enter a valid postal code")]
+        public string postalCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Country is required")]
+        public string country { get; set; } = string.Empty;
+    }
+}
