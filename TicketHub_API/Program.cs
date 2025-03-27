8de0e767a9c7aed65bd35f6b9bce45035a67fd7a
Nasfurion <diego.amadio93@gmail.com>
2025-03-27 13:37:31 -0300

    Troubleshooting_2

--- TicketHub_API/Program.cs
+++ TicketHub_API/Program.cs
@@ -1,9 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Add user secrets
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
