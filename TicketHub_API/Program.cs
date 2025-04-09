e3e52bfd294cea9f5b3136fd4b97a8fc00c1049d
nasfurion <you@example.com>
2025-04-09 09:41:44 -0300

    CORS Policy

--- TicketHub_API/Program.cs
+++ TicketHub_API/Program.cs
@@ -1,31 +1,26 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-
-// Add CORS policy
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("https://thankful-hill-06f44e50f.6.azurestaticapps.net/")
-                          .AllowAnyHeader()
-                          .AllowAnyMethod());
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://thankful-hill-06f44e50f.6.azurestaticapps.net");
+                      });
 });
 
+// services.AddResponseCaching();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
 
-// Use the CORS policy
-app.UseCors("AllowSpecificOrigin");
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
